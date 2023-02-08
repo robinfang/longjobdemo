5324ac0f0ad36166e16bd6e83bd074d1e8455c18
robinfang <thefmajia@gmail.com>
2023-02-08 15:30:27 +0800

    更新注释

--- app.py
+++ app.py
@@ -14,11 +14,20 @@ longjob.createTable()
 
 @app.route("/")
 def hello_world():
-    executor.submit_stored("demo", longjob.longjob)
-    return "<p>Hello, World!</p>"
+    """调用longjob.longjob函数，该函数运行15秒，但是这里不会阻塞而是立刻返回响应
+
+    """    
+    executor.submit_stored("demo", longjob.longjob)  # demo可以被看做是这个任务的id，之后可以用来查询任务是否完成
+    return "<p>Hello, World!</p>" # 实际使用时可以把任务id返回给前端供查询用，你后台只允许一个计算任务的话可以不需要
 
 @app.route('/status')
 def get_status():
+    """用于查询计算任务状态，可以在前端定时反复查询，然后成功后从数据库取计算结果
+
+    Returns:
+        json字符串: 任务已经完成则返回{"status": "done", 'result': null}，因为longjob.longjob没返回值所以变成了null
+                    任务未完成则显示{"status": "RUNNING"}
+    """
     if not executor.futures.done("demo"):
         return jsonify({"status": executor.futures._state("demo")})
     future = executor.futures.pop("demo")
